a6d797156e8368a3a30ec1df2812259ee6a82bbf
Joni M Salmi <josalmi@melkinkari.cs.helsinki.fi>
2013-12-16 14:25:02 +0200

    Eka dedis

---
+++ swagman/src/main/java/swagman/App.java
@@ -0,0 +1,13 @@
+package swagman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
